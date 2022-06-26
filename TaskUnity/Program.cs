b6a452a9247ddf52c0c2f18b646964733ebff5c1
Mardatein <butjamaks@mail.ru>
2022-06-26 17:03:49 +0300

    remove test 7

--- TaskUnity/Program.cs
+++ TaskUnity/Program.cs
@@ -298,7 +298,6 @@ namespace TaskExam
         {
             AssertEqual(FormatPrettyCoins(0, ' '), "0");
             AssertEqual(FormatPrettyCoins(5, ' '), "5");
-            AssertEqual(FormatPrettyCoins(7, ' '), "7");
             AssertEqual(FormatPrettyCoins(99, ' '), "99");
             AssertEqual(FormatPrettyCoins(100, ' '), "100");
             AssertEqual(FormatPrettyCoins(1000, ' '), "1 000");
